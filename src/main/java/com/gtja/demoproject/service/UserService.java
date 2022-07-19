e5934a285afba5a029a7bc424f902f56edd2bc37
shanshu <jsszkd@163.com>
2022-07-19 23:39:13 +0800

    测试分支

--- src/main/java/com/gtja/demoproject/service/UserService.java
+++ src/main/java/com/gtja/demoproject/service/UserService.java
@@ -1,4 +1,10 @@
 package com.gtja.demoproject.service;
 
+import com.gtja.demoproject.domain.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> listUser();
+
 }
--- src/main/java/com/gtja/demoproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/gtja/demoproject/service/impl/UserServiceImpl.java
@@ -1,6 +1,13 @@
 package com.gtja.demoproject.service.impl;
 
+import com.gtja.demoproject.domain.User;
 import com.gtja.demoproject.service.UserService;
 
+import java.util.List;
+
 public class UserServiceImpl implements UserService {
+    @Override
+    public List<User> listUser() {
+        return null;
+    }
 }
