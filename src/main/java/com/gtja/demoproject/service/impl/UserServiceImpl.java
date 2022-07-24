e3233d1ea37999298719275c778555132b163b9e
shanshu <jsszkd@163.com>
2022-07-24 13:28:05 +0800

    测试merge

--- src/main/java/com/gtja/demoproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/gtja/demoproject/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Override
     public List<User> listUser() {
+        System.out.println("this is main");
         return null;
     }
 }
